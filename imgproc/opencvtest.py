80a36a680927a2615ce3d4dce96286e19b0a8f67
Chuck Wilson <chuck.wilson@gmail.com>
2013-02-05 14:21:30 +0000

    More updates, still testing phase, no specific updates

--- imgproc/opencvtest.py
+++ imgproc/opencvtest.py
@@ -4,22 +4,26 @@
 ###       as it pops open a window
 
 import cv
-capture=cv.CaptureFromCAM(1)
 
+capture=cv.CaptureFromCAM(0)
+count=0
 #Load the haar cascade
 #hc = cv.Load("/opt/ros/fuerte/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml")
+#hc = cv.Load("/usr/share/opencv/haarcascades/haarcascade_frontalface_default.xml")
 
 def repeat():
+	global count
 	image=cv.QueryFrame(capture)
 	#Detect face in image
 #	face = cv.HaarDetectObjects(image, hc, cv.CreateMemStorage())
 #	for (x,y,w,h),n in face:
 #		cv.Rectangle(image, (x,y), (x+w,y+h), 255)
 #		print 'face found at: '+str(w)+','+str(h)
-	cv.ShowImage('Image_Window',image)
-	cv.WaitKey(50)
+#	cv.ShowImage('Image_Window',image)
+	cv.SaveImage('image_' + str(count) + '.jpeg', image)
+	cv.WaitKey(250)
+	count+=1
 
 
 while True:
 	repeat()
-
