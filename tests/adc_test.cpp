9e37815ad0d535935f1882d1e33c7443b784d64d
Chuck <chuck@chuck-laptop.(none)>
2013-03-27 03:55:23 -0400

    Added test stub for getAverage function (why didn't I have this before?)

--- tests/adc_test.cpp
+++ tests/adc_test.cpp
@@ -6,7 +6,12 @@ int main() {
   ADC_SPI adc(0, 0);
   
   while (1) {
+    std::cout << "Single value grab: ";
     std::cout << adc.getSingle() << std::endl;
+    sleep(1);
+    
+    std::cout << "Averaging of AVERAGING: ";
+    std::cout << adc.getAverage() << std::endl;
     sleep(1);
   }
   
