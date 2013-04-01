96df38cba03dd9f100b8360d00cd97608bcbf07b
Chuck <chuck@chuck-laptop.(none)>
2013-04-01 01:01:55 -0400

    Changed type member variable from int to enum

--- location/FSObject.h
+++ location/FSObject.h
@@ -4,40 +4,36 @@
 #include <string>
 #include <iostream>
 
+/// Defines the types used
+enum ObjectType { UNKNOWN, ROBOT, BEAM, COLUMN, STRUCTURE };
+
 class FSObject
 {
 protected:
-	int ID;
-	static int nextID;
-	std::string Name;
-	
-	int type;
-	/*
-		0 - Robot
-		1 - Beam
-		2 - Column
-		3 - Structure
-	   -1 - Unknown
-	*/
-
-	//Structure position represents the xyz of first member?
-	struct Coordinates 
-	{
-		float x;
-		float y;
-		float z;
-	};
-	Coordinates position;
-	float length, width, height;
-
+  int ID;
+  static int nextID;
+  std::string Name;
+  
+  enum ObjectType type;
+  
+  //Structure position represents the xyz of first member?
+  struct Coordinates 
+  {
+    float x;
+    float y;
+    float z;
+  };
+  Coordinates position;
+  float length, width, height;
+  
 public: 
-	FSObject(float,float,float,int,std::string name = "");
-	void get_Position(float* tx, float* ty, float* tz);
-	void set_Position(float,float,float);
-	int get_NextID();
-	void reset_ID(int);
-	int	get_ID();
-	//~FSObject();
+  FSObject(float,float,float,int,std::string name = "");
+  void get_Position(float* tx, float* ty, float* tz);
+  void set_Position(float,float,float);
+  int get_NextID();
+  void reset_ID(int);
+  int	get_ID();
+  //~FSObject();
 };
 
 
